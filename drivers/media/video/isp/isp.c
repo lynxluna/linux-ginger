ca10cbf892717817b0921739b80bfe4dba709738
Sakari Ailus <sakari.ailus@nokia.com>
2008-12-10 18:46:42 +0200

    omap3isp: Remove isp_get_xclk(), make isp_*_ctx() static
    Also remove prototypes for functions which do not exist
    
    Signed-off-by: Sakari Ailus <sakari.ailus@nokia.com>

--- drivers/media/video/isp/isp.c
+++ drivers/media/video/isp/isp.c
@@ -54,6 +54,10 @@
 //#define PRINTK(...) printk(__VA_ARGS__)
 #define PRINTK(...) do { } while (0)
 
+static void isp_save_ctx(void);
+
+static void isp_restore_ctx(void);
+
 static void isp_buf_init(void);
 
 struct iommu *isp_iommu;
@@ -576,51 +580,6 @@ u32 isp_set_xclk(u32 xclk, u8 xclksel)
 	return currentxclk;
 }
 EXPORT_SYMBOL(isp_set_xclk);
-
-/**
- * isp_get_xclk - Returns the frequency in Hz of the desired cam_xclk.
- * @xclksel: XCLK to retrieve (0 = A, 1 = B).
- *
- * This function returns the External Clock (XCLKA or XCLKB) value generated
- * by the ISP.
- **/
-u32 isp_get_xclk(u8 xclksel)
-{
-	u32 xclkdiv;
-	u32 xclk;
-
-	switch (xclksel) {
-	case 0:
-		xclkdiv = omap_readl(ISP_TCTRL_CTRL) & ISPTCTRL_CTRL_DIVA_MASK;
-		xclkdiv = xclkdiv >> ISPTCTRL_CTRL_DIVA_SHIFT;
-		break;
-	case 1:
-		xclkdiv = omap_readl(ISP_TCTRL_CTRL) & ISPTCTRL_CTRL_DIVB_MASK;
-		xclkdiv = xclkdiv >> ISPTCTRL_CTRL_DIVB_SHIFT;
-		break;
-	default:
-		DPRINTK_ISPCTRL("ISP_ERR: isp_get_xclk(): Invalid requested "
-						"xclk. Must be 0 (A) or 1 (B)."
-						"\n");
-		return -EINVAL;
-	}
-
-	switch (xclkdiv) {
-	case 0:
-	case 1:
-		xclk = 0;
-		break;
-	case 0x1f:
-		xclk = CM_CAM_MCLK_HZ;
-		break;
-	default:
-		xclk = CM_CAM_MCLK_HZ / xclkdiv;
-		break;
-	}
-
-	return xclk;
-}
-EXPORT_SYMBOL(isp_get_xclk);
 
 /**
  * isp_power_settings - Sysconfig settings, for Power Management.
@@ -1881,8 +1840,8 @@ EXPORT_SYMBOL(isp_try_fmt_cap);
  * isppreview_try_size, or ispresizer_try_size (depending on the pipeline
  * configuration) if there is an error.
  **/
-int isp_try_size(struct v4l2_pix_format *pix_input,
-					struct v4l2_pix_format *pix_output)
+static int isp_try_size(struct v4l2_pix_format *pix_input,
+			struct v4l2_pix_format *pix_output)
 {
 	int rval = 0;
 
@@ -2009,7 +1968,7 @@ EXPORT_SYMBOL(isp_try_fmt);
  * Routine for saving the context of each module in the ISP.
  * CCDC, HIST, H3A, PREV, RESZ and MMU.
  **/
-void isp_save_ctx(void)
+static void isp_save_ctx(void)
 {
 	isp_save_context(isp_reg_list);
 	ispccdc_save_context();
@@ -2019,7 +1978,6 @@ void isp_save_ctx(void)
 	isppreview_save_context();
 	ispresizer_save_context();
 }
-EXPORT_SYMBOL(isp_save_ctx);
 
 /**
  * isp_restore_ctx - Restores ISP, CCDC, HIST, H3A, PREV, RESZ & MMU context.
@@ -2027,7 +1985,7 @@ EXPORT_SYMBOL(isp_save_ctx);
  * Routine for restoring the context of each module in the ISP.
  * CCDC, HIST, H3A, PREV, RESZ and MMU.
  **/
-void isp_restore_ctx(void)
+static void isp_restore_ctx(void)
 {
 	isp_restore_context(isp_reg_list);
 	ispccdc_restore_context();
@@ -2037,7 +1995,6 @@ void isp_restore_ctx(void)
 	isppreview_restore_context();
 	ispresizer_restore_context();
 }
-EXPORT_SYMBOL(isp_restore_ctx);
 
 /**
  * isp_get - Adquires the ISP resource.
