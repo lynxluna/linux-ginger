d8c42e4d2f4207c2e55a82116fcaeb72745ff5fe
Fernando Guzman Lugo <x0095840@ti.com>
2009-06-16 15:44:01 -0500

    DSPBRIDGE: Reset MMU and DSP on board stop
    This patch resets MMU and DSP by writintg into those registers,
    this will solve MMU faults occured when the processor is stopped.
    
    Signed-off-by: Omar Ramirez Luna <omar.ramirez@ti.com>
    Signed-off-by: Fernando Guzman <x0095840@ti.com>

--- drivers/dsp/bridge/wmd/tiomap3430.c
+++ drivers/dsp/bridge/wmd/tiomap3430.c
@@ -850,6 +850,9 @@ static DSP_STATUS WMD_BRD_Stop(struct WMD_DEV_CONTEXT *hDevContext)
 		       (pPtAttrs->L2NumPages * sizeof(struct PageInfo)));
 	}
 	DBG_Trace(DBG_LEVEL6, "WMD_BRD_Stop - End ****** \n");
+	HW_RST_Reset(resources.dwPrmBase, HW_RST1_IVA2);
+	HW_RST_Reset(resources.dwPrmBase, HW_RST2_IVA2);
+
 	return status;
 }
 
@@ -912,7 +915,7 @@ static DSP_STATUS WMD_BRD_Delete(struct WMD_DEV_CONTEXT *hDevContext)
 		memset((u8 *)pPtAttrs->pgInfo, 0x00,
 			(pPtAttrs->L2NumPages * sizeof(struct PageInfo)));
 	}
-	DBG_Trace(DBG_LEVEL6, "WMD_BRD_Stop - End ****** \n");
+	DBG_Trace(DBG_LEVEL6, "WMD_BRD_Delete - End ****** \n");
 	HW_RST_Reset(resources.dwPrmBase, HW_RST1_IVA2);
 	HW_RST_Reset(resources.dwPrmBase, HW_RST2_IVA2);
 
