01113c47e94326634229b7baa28e952950cb9704
Sergio Aguirre <saaguirre@ti.com>
2009-07-01 18:42:05 -0500

    omap3isp: Add callback for Resizer wrapper
    This adds the possibility to add an external callback function for
    resizer. User currently is TI Resizer Wrapper.
    
    Signed-off-by: Sergio Aguirre <saaguirre@ti.com>
    Signed-off-by: Dominic Curran <dcurran@ti.com>

--- drivers/media/video/isp/isp.c
+++ drivers/media/video/isp/isp.c
@@ -422,6 +422,12 @@ int isp_set_callback(struct device *dev, enum isp_callback_type type,
 		isp_reg_or(dev, OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0ENABLE,
 			   IRQ0ENABLE_PRV_DONE_IRQ);
 		break;
+	case CBK_RESZ_DONE:
+		isp_reg_writel(dev, IRQ0ENABLE_RSZ_DONE_IRQ,
+			       OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0STATUS);
+		isp_reg_or(dev, OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0ENABLE,
+			   IRQ0ENABLE_RSZ_DONE_IRQ);
+		break;
 	default:
 		break;
 	}
@@ -453,6 +459,10 @@ int isp_unset_callback(struct device *dev, enum isp_callback_type type)
 	case CBK_PREV_DONE:
 		isp_reg_and(dev, OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0ENABLE,
 			    ~IRQ0ENABLE_PRV_DONE_IRQ);
+		break;
+	case CBK_RESZ_DONE:
+		isp_reg_and(dev, OMAP3_ISP_IOMEM_MAIN, ISP_IRQ0ENABLE,
+			    ~IRQ0ENABLE_RSZ_DONE_IRQ);
 		break;
 	default:
 		break;
@@ -1014,8 +1024,16 @@ static irqreturn_t isp_isr(int irq, void *_pdev)
 		}
 	}
 
-	if (irqstatus & RESZ_DONE && CCDC_PREV_RESZ_CAPTURE(isp))
-		isp_buf_process(dev, bufs);
+	if (irqstatus & RESZ_DONE) {
+		if (irqdis->isp_callbk[CBK_RESZ_DONE])
+			irqdis->isp_callbk[CBK_RESZ_DONE](
+				RESZ_DONE,
+				irqdis->isp_callbk_arg1[CBK_RESZ_DONE],
+				irqdis->isp_callbk_arg2[CBK_RESZ_DONE]);
+		else if (CCDC_PREV_RESZ_CAPTURE(isp)) {
+			isp_buf_process(dev, bufs);
+		}
+	}
 
 	if ((irqstatus & CCDC_VD1) && RAW_CAPTURE(isp)) {
 		/*
