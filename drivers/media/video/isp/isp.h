fee223735c98a75332425febb06f07136f1e59b9
David Cohen <david.cohen@nokia.com>
2009-07-29 18:18:52 +0300

    omap3isp: Adding DMA support for histogram
    This patch adds DMA support for histogram. It's necessary in order to
    speed up the buffer processing after new data is ready.
    PIO will be forced if no DMA buffer or channel is available.
    
    Signed-off-by: David Cohen <david.cohen@nokia.com>

--- drivers/media/video/isp/isp.h
+++ drivers/media/video/isp/isp.h
@@ -361,6 +361,8 @@ struct isp_device {
 	struct iommu *iommu;
 };
 
+void omap34xx_isp_hist_dma_done(struct device *dev);
+
 void isp_flush(struct device *dev);
 
 void isp_start(struct device *dev);
