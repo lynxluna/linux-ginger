f9c9cc4d18944adef9b76c5c14ab0846974463dd
Laurent Pinchart <laurent.pinchart@ideasonboard.com>
2009-08-04 14:10:18 +0300

    omap3isp: Disable ISP auto-idle
    ISP auto-idle messes up the image for a still unknown reason. A color tint
    is observed randomly in the viewfinder, with the actual color varying between
    devices (blue, magenta, yellow, ...).
    
    Investigation showed that inserting printk statements in the ISP interrupt
    handler would make the problem even worse, so this might be timing related.
    The user-space camera daemon could also be responsible for setting wrong image
    settings (such as white balance).
    
    Raw images should be captured at different points in the CCDC-Previewer-Resizer
    pipeline to find out where the problem originates.
    
    Disable ISP auto-idle for now until the root cause of the problem can be found
    and fixed.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>

--- drivers/media/video/isp/isp.c
+++ drivers/media/video/isp/isp.c
@@ -454,7 +454,7 @@ EXPORT_SYMBOL(isp_set_xclk);
 static void isp_power_settings(struct device *dev, int idle)
 {
 	if (idle) {
-		isp_reg_writel(dev, ISP_SYSCONFIG_AUTOIDLE |
+		isp_reg_writel(dev,
 			       (ISP_SYSCONFIG_MIDLEMODE_SMARTSTANDBY <<
 				ISP_SYSCONFIG_MIDLEMODE_SHIFT),
 			       OMAP3_ISP_IOMEM_MAIN, ISP_SYSCONFIG);
@@ -474,7 +474,7 @@ static void isp_power_settings(struct device *dev, int idle)
 			       ISP_CTRL);
 
 	} else {
-		isp_reg_writel(dev, ISP_SYSCONFIG_AUTOIDLE |
+		isp_reg_writel(dev,
 			       (ISP_SYSCONFIG_MIDLEMODE_FORCESTANDBY <<
 				ISP_SYSCONFIG_MIDLEMODE_SHIFT),
 			       OMAP3_ISP_IOMEM_MAIN, ISP_SYSCONFIG);
