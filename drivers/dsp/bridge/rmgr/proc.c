abb65a6b84a34e97ac3a4ac01abf4d1c75457f9d
Hiroshi DOYU <hiroshi.doyu@nokia.com>
2009-08-11 20:08:54 -0500

    DSPBRIDGE: add checking 128 byte alignment for dsp cache line size
    A buffer shared with MPU and DSP has to be aligned on both cache line
    size to avoid memory corrupton with some DSP cache operations. Since
    there's no way for dspbridge to know how the shared buffer will be
    used like: "read-only", "write-only", "rw" through its life span, any
    shared buffer passed to DSP should be on this alignment. This patch
    adds checking those shared buffer alignement in bridgedriver cache
    operations and prevents userland applications from causing the above
    memory corruption.
    
    Please refer to:
    https://omapzoom.org/gf/download/docmanfileversion/52/985/DSP_cache.pdf
    
    [omar ramirez: extended the check into PROC_Map, created Kconfig option]
    
    Signed-off-by: Hiroshi DOYU <hiroshi.doyu@nokia.com>
    Signed-off-by: Omar Ramirez Luna <omar.ramirez@ti.com>

--- drivers/dsp/bridge/rmgr/proc.c
+++ drivers/dsp/bridge/rmgr/proc.c
@@ -162,6 +162,8 @@
 #define PWR_TIMEOUT	 500	/* Sleep/wake timout in msec */
 #define EXTEND	      "_EXT_END"	/* Extmem end addr in DSP binary */
 
+#define DSP_CACHE_SIZE 128 /* DSP cacheline size */
+
 extern char *iva_img;
 /* The PROC_OBJECT structure.   */
 struct PROC_OBJECT {
@@ -752,6 +754,15 @@ DSP_STATUS PROC_FlushMemory(DSP_HPROCESSOR hProcessor, void *pMpuAddr,
 	struct PROC_OBJECT *pProcObject = (struct PROC_OBJECT *)hProcessor;
 	DBC_Require(cRefs > 0);
 
+#ifdef CONFIG_BRIDGE_CHECK_ALIGN_128
+	if (!IS_ALIGNED((u32)pMpuAddr, DSP_CACHE_SIZE) ||
+	    !IS_ALIGNED(ulSize, DSP_CACHE_SIZE)) {
+		pr_err("%s: Invalid alignment %p %x\n",
+			__func__, pMpuAddr, ulSize);
+		return DSP_EALIGNMENT;
+	}
+#endif /* CONFIG_BRIDGE_CHECK_ALIGN_128 */
+
 	GT_4trace(PROC_DebugMask, GT_ENTER,
 		 "Entered PROC_FlushMemory, args:\n\t"
 		 "hProcessor: 0x%x pMpuAddr: 0x%x ulSize 0x%x, ulFlags 0x%x\n",
@@ -789,6 +800,16 @@ DSP_STATUS PROC_InvalidateMemory(DSP_HPROCESSOR hProcessor, void *pMpuAddr,
 		 "Entered PROC_InvalidateMemory, args:\n\t"
 		 "hProcessor: 0x%x pMpuAddr: 0x%x ulSize 0x%x\n", hProcessor,
 		 pMpuAddr, ulSize);
+
+#ifdef CONFIG_BRIDGE_CHECK_ALIGN_128
+	if (!IS_ALIGNED((u32)pMpuAddr, DSP_CACHE_SIZE) ||
+	    !IS_ALIGNED(ulSize, DSP_CACHE_SIZE)) {
+		pr_err("%s: Invalid alignment %p %x\n",
+			__func__, pMpuAddr, ulSize);
+		return DSP_EALIGNMENT;
+	}
+#endif /* CONFIG_BRIDGE_CHECK_ALIGN_128 */
+
 	if (MEM_IsValidHandle(pProcObject, PROC_SIGNATURE)) {
 		(void)SYNC_EnterCS(hProcLock);
 		MEM_FlushCache(pMpuAddr, ulSize, FlushMemType);
@@ -1401,6 +1422,16 @@ DSP_STATUS PROC_Map(DSP_HPROCESSOR hProcessor, void *pMpuAddr, u32 ulSize,
 		 "hProcessor %x, pMpuAddr %x, ulSize %x, pReqAddr %x, "
 		 "ulMapAttr %x, ppMapAddr %x\n", hProcessor, pMpuAddr, ulSize,
 		 pReqAddr, ulMapAttr, ppMapAddr);
+
+#ifdef CONFIG_BRIDGE_CHECK_ALIGN_128
+	if (!IS_ALIGNED((u32)pMpuAddr, DSP_CACHE_SIZE) ||
+	    !IS_ALIGNED(ulSize, DSP_CACHE_SIZE)) {
+		pr_err("%s: Invalid alignment %p %x\n",
+			__func__, pMpuAddr, ulSize);
+		return DSP_EALIGNMENT;
+	}
+#endif /* CONFIG_BRIDGE_CHECK_ALIGN_128 */
+
 	/* Calculate the page-aligned PA, VA and size */
 	vaAlign = PG_ALIGN_LOW((u32) pReqAddr, PG_SIZE_4K);
 	paAlign = PG_ALIGN_LOW((u32) pMpuAddr, PG_SIZE_4K);
