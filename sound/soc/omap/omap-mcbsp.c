3a684e77aae4d0eba76071766f7612bdad2c6536
Jane Wang <jwang@ti.com>
2009-09-21 12:12:59 -0500

    Audio: Fix audio suspend/resume failure for RETENTION mode
    1. disable/enable McBSP function clock for suspend/resume
    2. Disable SDMA channel
    in order to enter target state while suspend and resume sucessfully afterwards.
    
    Signed-off-by: Jane Wang <jwang@ti.com>
    
    Modified according to commit id f621422 of lo-sync tree

--- sound/soc/omap/omap-mcbsp.c
+++ sound/soc/omap/omap-mcbsp.c
@@ -227,17 +227,29 @@ static int omap_mcbsp_dai_trigger(struct snd_pcm_substream *substream, int cmd,
 
 	switch (cmd) {
 	case SNDRV_PCM_TRIGGER_START:
-	case SNDRV_PCM_TRIGGER_RESUME:
 	case SNDRV_PCM_TRIGGER_PAUSE_RELEASE:
 		mcbsp_data->active++;
 		omap_mcbsp_start(mcbsp_data->bus_id, play, !play);
 		break;
 
+	case SNDRV_PCM_TRIGGER_RESUME:
+		if (!mcbsp_data->active++) {
+			omap_mcbsp_enable_fclk(mcbsp_data->bus_id);
+			omap_mcbsp_start(mcbsp_data->bus_id, play, !play);
+		}
+		break;
+
 	case SNDRV_PCM_TRIGGER_STOP:
-	case SNDRV_PCM_TRIGGER_SUSPEND:
 	case SNDRV_PCM_TRIGGER_PAUSE_PUSH:
 		omap_mcbsp_stop(mcbsp_data->bus_id, play, !play);
 		mcbsp_data->active--;
+		break;
+
+	case SNDRV_PCM_TRIGGER_SUSPEND:
+		if (!--mcbsp_data->active) {
+			omap_mcbsp_stop(mcbsp_data->bus_id, play, !play);
+			omap_mcbsp_disable_fclk(mcbsp_data->bus_id);
+		}
 		break;
 	default:
 		err = -EINVAL;
