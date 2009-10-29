1e719452132dc896c9e1090bc44b87b4e65bad45
Andy Shevchenko <ext-andriy.shevchenko@nokia.com>
2009-10-29 14:53:19 -0600

    DSPBRIDGE: OSAL: Clean up dbreg.h and move it
    * Clean up dbreg.h - there are plenty of unused definitions
    * Move necessary constants to the dbdefs.h
    * Remove dbreg.h inclusion from other files
    
    Signed-off-by: Andy Shevchenko <ext-andriy.shevchenko@nokia.com>
    Signed-off-by: Omar Ramirez Luna <omar.ramirez@ti.com>

--- drivers/dsp/bridge/rmgr/drv_interface.c
+++ drivers/dsp/bridge/rmgr/drv_interface.c
@@ -79,7 +79,6 @@
 #include <dspbridge/wcdioctl.h>
 #include <dspbridge/_dcd.h>
 #include <dspbridge/dspdrv.h>
-#include <dspbridge/dbreg.h>
 
 /*  ----------------------------------- Resource Manager */
 #include <dspbridge/pwr.h>
@@ -96,7 +95,6 @@
 #include <dspbridge/dev.h>
 #include <dspbridge/drvdefs.h>
 #include <dspbridge/drv.h>
-#include <dspbridge/dbreg.h>
 #endif
 
 #ifdef CONFIG_BRIDGE_DVFS
@@ -129,7 +127,6 @@ static s32 driver_major;
 static s32 driver_minor;
 static char *base_img;
 char *iva_img;
-static char *num_procs = "C55=1";
 static s32 shm_size = 0x500000;	/* 5 MB */
 static u32 phys_mempool_base;
 static u32 phys_mempool_size;
@@ -350,7 +347,6 @@ static int __devinit omap34xx_bridge_probe(struct platform_device *pdev)
 		REG_SetValue(AUTOSTART, (u8 *)&temp, sizeof(temp));
 		REG_SetValue(DEFEXEC, (u8 *) "\0", (u32)2);
 	}
-	REG_SetValue(NUMPROCS, (u8 *) num_procs, strlen(num_procs) + 1);
 
 	if (shm_size >= 0x10000) {	/* 64 KB */
 		initStatus = REG_SetValue(SHMSIZE, (u8 *)&shm_size,
@@ -369,21 +365,12 @@ static int __devinit omap34xx_bridge_probe(struct platform_device *pdev)
 		phys_mempool_size = pdata->phys_mempool_size;
 	}
 
-	if (phys_mempool_base > 0x0) {
-		initStatus = REG_SetValue(PHYSMEMPOOLBASE,
-					(u8 *)&phys_mempool_base,
-					sizeof(phys_mempool_base));
-	}
 	GT_1trace(driverTrace, GT_7CLASS, "phys_mempool_base = 0x%x \n",
 		 phys_mempool_base);
 
-	if (phys_mempool_size > 0x0) {
-		initStatus = REG_SetValue(PHYSMEMPOOLSIZE,
-					(u8 *)&phys_mempool_size,
-					sizeof(phys_mempool_size));
-	}
 	GT_1trace(driverTrace, GT_7CLASS, "phys_mempool_size = 0x%x\n",
 		 phys_mempool_base);
+
 	if ((phys_mempool_base > 0x0) && (phys_mempool_size > 0x0))
 		MEM_ExtPhysPoolInit(phys_mempool_base, phys_mempool_size);
 	if (tc_wordswapon) {
