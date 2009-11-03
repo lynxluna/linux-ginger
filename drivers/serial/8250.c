87eef228aa26e084acadefca73bf7deac3e63206
Vikram Pandita <vikram.pandita@ti.com>
2009-11-03 12:24:21 -0600

    OMAP3630 : 8250: fix RX fifo access
    On 3630 the IP revision of UART has changed.
    with this change, an access of empty fifo causes an abort.
    so introduce a check for non-empty fifo before accessing the RX fifo.
    
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>
    Signed-off-by: Allen Pais <allen.pais@ti.com>

--- drivers/serial/8250.c
+++ drivers/serial/8250.c
@@ -1245,7 +1245,8 @@ static void autoconfig(struct uart_8250_port *up, unsigned int probeflags)
 #endif
 	serial_outp(up, UART_MCR, save_mcr);
 	serial8250_clear_fifos(up);
-	serial_in(up, UART_RX);
+	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
+		serial_in(up, UART_RX);
 	if (up->capabilities & UART_CAP_UUE)
 		serial_outp(up, UART_IER, UART_IER_UUE);
 	else
@@ -1288,8 +1289,9 @@ static void autoconfig_irq(struct uart_8250_port *up)
 			    UART_MCR_DTR | UART_MCR_RTS | UART_MCR_OUT2);
 	}
 	serial_outp(up, UART_IER, 0x0f);	/* enable all intrs */
-	(void)serial_inp(up, UART_LSR);
-	(void)serial_inp(up, UART_RX);
+	/*(void)serial_inp(up, UART_LSR); */
+	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
+		(void)serial_inp(up, UART_RX);
 	(void)serial_inp(up, UART_IIR);
 	(void)serial_inp(up, UART_MSR);
 	serial_outp(up, UART_TX, 0xFF);
@@ -1983,8 +1985,9 @@ static int serial8250_startup(struct uart_port *port)
 	/*
 	 * Clear the interrupt registers.
 	 */
-	(void) serial_inp(up, UART_LSR);
-	(void) serial_inp(up, UART_RX);
+	/*(void) serial_inp(up, UART_LSR); */
+	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
+		(void) serial_inp(up, UART_RX);
 	(void) serial_inp(up, UART_IIR);
 	(void) serial_inp(up, UART_MSR);
 
@@ -2140,8 +2143,9 @@ dont_test_tx_en:
 	 * saved flags to avoid getting false values from polling
 	 * routines or the previous session.
 	 */
-	serial_inp(up, UART_LSR);
-	serial_inp(up, UART_RX);
+	/*serial_inp(up, UART_LSR); */
+	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
+		serial_inp(up, UART_RX);
 	serial_inp(up, UART_IIR);
 	serial_inp(up, UART_MSR);
 	up->lsr_saved_flags = 0;
@@ -2207,7 +2211,8 @@ static void serial8250_shutdown(struct uart_port *port)
 	 * Read data port to reset things, and then unlink from
 	 * the IRQ chain.
 	 */
-	(void) serial_in(up, UART_RX);
+	if (serial_inp(up, UART_LSR) & UART_LSR_DR)
+		(void) serial_in(up, UART_RX);
 
 	del_timer_sync(&up->timer);
 	up->timer.function = serial8250_timeout;
