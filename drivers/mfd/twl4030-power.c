39a162a9284655209e174e8fb9b10cfc95351370
Ghorai, Sukumar <s-ghorai@ti.com>
2009-11-09 19:37:24 +0530

    Added ZOOM3 to board checks for enabling audio
    1. Added ZOOM3 to board checks for enabling audio
    
     2.Added MACH_OMAP_ZOOM3 to select SOC_MCBSP & SOC_TWL4030
    
    Signed-off-by: Ghorai, Sukumar <s-ghorai@ti.com>

--- drivers/mfd/twl4030-power.c
+++ drivers/mfd/twl4030-power.c
@@ -190,7 +190,8 @@ static int __init config_wakeup_sequence(u8 address)
 	err |= twl4030_i2c_write_u8(TWL4030_MODULE_PM_MASTER, LVL_WAKEUP,
 					R_P3_SW_EVENTS);
 
-	if (machine_is_omap_3430sdp() || machine_is_omap_ldp()) {
+	if (machine_is_omap_3430sdp() || machine_is_omap_ldp() ||
+		machine_is_omap_zoom2() || machine_is_omap_zoom3()) {
 		/* Disabling AC charger effect on sleep-active transitions */
 		err = twl4030_i2c_read_u8(TWL4030_MODULE_PM_MASTER, &data,
 					R_CFG_P1_TRANSITION);
