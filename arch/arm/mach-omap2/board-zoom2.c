d6d456d0cd9420d23569e926c40351beaa2769c4
vikram pandita <vikram.pandita@ti.com>
2009-11-10 22:47:24 +0000

    omap: zoom2: split board file for software reuse
    Split zoom2 board file into:
    base board file and a board-zoom-peripherals.c file
    
    that way the same peripherals file can be reused for: zoom3 and sdp3630
    
    Following list shows the commonality across the three platforms and hence the
    case for software reuse:
    
    Peripheral    zoom2    zoom3  sdp3630
    ---------------------------------------
    Ethernet        smsc    smsc    smc
    NOR             n/a     n/a     B
    Onenand         n/a     n/a     B
    HDMI            A       A       B (present on different i2c)
    NAND            A       A       A (same nand)
    SDRAM           A       A       A (same sdram)
    Keypad          A       A       A (same twl)
    Camera          A       A       A (same sensor can be mounted)
    LCD Display     A       A       A (same wvga display)
    OPPs            A       A       A (same chip feature)
    Audio           A       A       A (same audio via twl5030)
    
    Signed-off-by: Vikram Pandita <vikram.pandita@ti.com>
    Signed-off-by: Manjunatha GK <manjugk@ti.com>

--- arch/arm/mach-omap2/board-zoom2.c
+++ arch/arm/mach-omap2/board-zoom2.c
@@ -14,426 +14,59 @@
 #include <linux/platform_device.h>
 #include <linux/input.h>
 #include <linux/input/matrix_keypad.h>
+#include <linux/gpio.h>
 #include <linux/i2c/twl4030.h>
 #include <linux/regulator/machine.h>
-#include <linux/interrupt.h>
-#include <linux/synaptics_i2c_rmi.h>
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
+#include <asm/mach/map.h>
 
 #include <plat/common.h>
 #include <plat/usb.h>
-#include <plat/keypad.h>
 #include <plat/mux.h>
-#include <plat/gpio.h>
-
-#include <mach/board-zoom.h>
 
 #include "mmc-twl4030.h"
 #include "sdram-micron-mt46h32m32lf-6.h"
 #include "omap3-opp.h"
 
-#define OMAP_SYNAPTICS_GPIO		163
-
-/* Zoom2 has Qwerty keyboard*/
-static int board_keymap[] = {
-	KEY(0, 0, KEY_E),
-	KEY(1, 0, KEY_R),
-	KEY(2, 0, KEY_T),
-	KEY(3, 0, KEY_HOME),
-	KEY(6, 0, KEY_I),
-	KEY(7, 0, KEY_LEFTSHIFT),
-	KEY(0, 1, KEY_D),
-	KEY(1, 1, KEY_F),
-	KEY(2, 1, KEY_G),
-	KEY(3, 1, KEY_SEND),
-	KEY(6, 1, KEY_K),
-	KEY(7, 1, KEY_ENTER),
-	KEY(0, 2, KEY_X),
-	KEY(1, 2, KEY_C),
-	KEY(2, 2, KEY_V),
-	KEY(3, 2, KEY_END),
-	KEY(6, 2, KEY_DOT),
-	KEY(7, 2, KEY_CAPSLOCK),
-	KEY(0, 3, KEY_Z),
-	KEY(1, 3, KEY_KPPLUS),
-	KEY(2, 3, KEY_B),
-	KEY(3, 3, KEY_F1),
-	KEY(6, 3, KEY_O),
-	KEY(7, 3, KEY_SPACE),
-	KEY(0, 4, KEY_W),
-	KEY(1, 4, KEY_Y),
-	KEY(2, 4, KEY_U),
-	KEY(3, 4, KEY_F2),
-	KEY(4, 4, KEY_VOLUMEUP),
-	KEY(6, 4, KEY_L),
-	KEY(7, 4, KEY_LEFT),
-	KEY(0, 5, KEY_S),
-	KEY(1, 5, KEY_H),
-	KEY(2, 5, KEY_J),
-	KEY(3, 5, KEY_F3),
-	KEY(5, 5, KEY_VOLUMEDOWN),
-	KEY(6, 5, KEY_M),
-	KEY(4, 5, KEY_ENTER),
-	KEY(7, 5, KEY_RIGHT),
-	KEY(0, 6, KEY_Q),
-	KEY(1, 6, KEY_A),
-	KEY(2, 6, KEY_N),
-	KEY(3, 6, KEY_BACKSPACE),
-	KEY(6, 6, KEY_P),
-	KEY(7, 6, KEY_UP),
-	KEY(6, 7, KEY_SELECT),
-	KEY(7, 7, KEY_DOWN),
-	KEY(0, 7, KEY_PROG1),	/*MACRO 1 <User defined> */
-	KEY(1, 7, KEY_PROG2),	/*MACRO 2 <User defined> */
-	KEY(2, 7, KEY_PROG3),	/*MACRO 3 <User defined> */
-	KEY(3, 7, KEY_PROG4),	/*MACRO 4 <User defined> */
-	0
-};
-
-static struct matrix_keymap_data board_map_data = {
-	.keymap			= board_keymap,
-	.keymap_size		= ARRAY_SIZE(board_keymap),
-};
-
-static struct twl4030_keypad_data zoom2_kp_twl4030_data = {
-	.keymap_data	= &board_map_data,
-	.rows		= 8,
-	.cols		= 8,
-	.rep		= 1,
-};
-
-static struct omap_board_config_kernel zoom2_config[] __initdata = {
-};
-
-static struct regulator_consumer_supply zoom2_vmmc1_supply = {
-	.supply		= "vmmc",
-};
-
-static struct regulator_consumer_supply zoom2_vsim_supply = {
-	.supply		= "vmmc_aux",
-};
-
-static struct regulator_consumer_supply zoom2_vmmc2_supply = {
-	.supply		= "vmmc",
-};
-
-/* VMMC1 for OMAP VDD_MMC1 (i/o) and MMC1 card */
-static struct regulator_init_data zoom2_vmmc1 = {
-	.constraints = {
-		.min_uV			= 1850000,
-		.max_uV			= 3150000,
-		.valid_modes_mask	= REGULATOR_MODE_NORMAL
-					| REGULATOR_MODE_STANDBY,
-		.valid_ops_mask		= REGULATOR_CHANGE_VOLTAGE
-					| REGULATOR_CHANGE_MODE
-					| REGULATOR_CHANGE_STATUS,
-	},
-	.num_consumer_supplies  = 1,
-	.consumer_supplies      = &zoom2_vmmc1_supply,
-};
-
-/* VMMC2 for MMC2 card */
-static struct regulator_init_data zoom2_vmmc2 = {
-	.constraints = {
-		.min_uV			= 1850000,
-		.max_uV			= 1850000,
-		.apply_uV		= true,
-		.valid_modes_mask	= REGULATOR_MODE_NORMAL
-					| REGULATOR_MODE_STANDBY,
-		.valid_ops_mask		= REGULATOR_CHANGE_MODE
-					| REGULATOR_CHANGE_STATUS,
-	},
-	.num_consumer_supplies  = 1,
-	.consumer_supplies      = &zoom2_vmmc2_supply,
-};
-
-/* VSIM for OMAP VDD_MMC1A (i/o for DAT4..DAT7) */
-static struct regulator_init_data zoom2_vsim = {
-	.constraints = {
-		.min_uV			= 1800000,
-		.max_uV			= 3000000,
-		.valid_modes_mask	= REGULATOR_MODE_NORMAL
-					| REGULATOR_MODE_STANDBY,
-		.valid_ops_mask		= REGULATOR_CHANGE_VOLTAGE
-					| REGULATOR_CHANGE_MODE
-					| REGULATOR_CHANGE_STATUS,
-	},
-	.num_consumer_supplies  = 1,
-	.consumer_supplies      = &zoom2_vsim_supply,
-};
-
-static struct twl4030_hsmmc_info mmc[] __initdata = {
-	{
-		.mmc		= 1,
-		.wires		= 4,
-		.gpio_wp	= -EINVAL,
-		.power_saving	= true,
-	},
-	{
-		.mmc		= 2,
-		.wires		= 4,
-		.gpio_wp	= -EINVAL,
-		.power_saving	= true,
-	},
-	{}      /* Terminator */
-};
-
-static int zoom2_twl_gpio_setup(struct device *dev,
-		unsigned gpio, unsigned ngpio)
-{
-	/* gpio + 0 is "mmc0_cd" (input/IRQ),
-	 * gpio + 1 is "mmc1_cd" (input/IRQ)
-	 */
-	mmc[0].gpio_cd = gpio + 0;
-	mmc[1].gpio_cd = gpio + 1;
-	twl4030_mmc_init(mmc);
-
-	/* link regulators to MMC adapters ... we "know" the
-	 * regulators will be set up only *after* we return.
-	*/
-	zoom2_vmmc1_supply.dev = mmc[0].dev;
-	zoom2_vsim_supply.dev = mmc[0].dev;
-	zoom2_vmmc2_supply.dev = mmc[1].dev;
-
-	return 0;
-}
-
-
-static int zoom2_batt_table[] = {
-/* 0 C*/
-30800, 29500, 28300, 27100,
-26000, 24900, 23900, 22900, 22000, 21100, 20300, 19400, 18700, 17900,
-17200, 16500, 15900, 15300, 14700, 14100, 13600, 13100, 12600, 12100,
-11600, 11200, 10800, 10400, 10000, 9630,  9280,  8950,  8620,  8310,
-8020,  7730,  7460,  7200,  6950,  6710,  6470,  6250,  6040,  5830,
-5640,  5450,  5260,  5090,  4920,  4760,  4600,  4450,  4310,  4170,
-4040,  3910,  3790,  3670,  3550
-};
-
-static struct twl4030_bci_platform_data zoom2_bci_data = {
-	.battery_tmp_tbl	= zoom2_batt_table,
-	.tblsize		= ARRAY_SIZE(zoom2_batt_table),
-};
-
-static struct twl4030_usb_data zoom2_usb_data = {
-	.usb_mode	= T2_USB_MODE_ULPI,
-};
-
-static void __init omap_zoom2_init_irq(void)
-{
-	omap_board_config = zoom2_config;
-	omap_board_config_size = ARRAY_SIZE(zoom2_config);
-	omap2_init_common_hw(mt46h32m32lf6_sdrc_params,
-				 mt46h32m32lf6_sdrc_params, omap3_mpu_rate_table,
-				 omap3_dsp_rate_table, omap3_l3_rate_table);
-	omap_init_irq();
-	omap_gpio_init();
-}
-
-static struct twl4030_gpio_platform_data zoom2_gpio_data = {
-	.gpio_base	= OMAP_MAX_GPIO_LINES,
-	.irq_base	= TWL4030_GPIO_IRQ_BASE,
-	.irq_end	= TWL4030_GPIO_IRQ_END,
-	.setup		= zoom2_twl_gpio_setup,
-};
-
-static struct twl4030_madc_platform_data zoom2_madc_data = {
-	.irq_line	= 1,
-};
-
-static struct twl4030_ins __initdata sleep_on_seq[] = {
-	/* Broadcast message to put res to sleep */
-	{MSG_BROADCAST(DEV_GRP_NULL, RES_GRP_ALL, RES_TYPE_R1, RES_TYPE2_R0,
-							RES_STATE_SLEEP), 2},
-};
-
-static struct twl4030_script sleep_on_script __initdata = {
-	.script	= sleep_on_seq,
-	.size	= ARRAY_SIZE(sleep_on_seq),
-	.flags	= TWL4030_SLEEP_SCRIPT,
-};
-
-static struct twl4030_ins wakeup_seq[] __initdata = {
-	/* Broadcast message to put res to active */
-	{MSG_BROADCAST(DEV_GRP_NULL, RES_GRP_ALL, RES_TYPE_R1, RES_TYPE2_R0,
-			RES_STATE_ACTIVE), 2},
-};
-
-static struct twl4030_script wakeup_script __initdata = {
-	.script = wakeup_seq,
-	.size   = ARRAY_SIZE(wakeup_seq),
-	.flags  = TWL4030_WAKEUP_SCRIPT,
-};
-
-static struct twl4030_ins wrst_seq[] __initdata = {
-/*
- * Reset twl4030.
- * Reset VDD1 regulator.
- * Reset VDD2 regulator.
- * Reset VPLL1 regulator.
- * Enable sysclk output.
- * Reenable twl4030.
- */
-	{MSG_SINGULAR(DEV_GRP_NULL, 0x1b, RES_STATE_OFF), 2},
-	{MSG_SINGULAR(DEV_GRP_P1, 0xf, RES_STATE_WRST), 15},
-	{MSG_SINGULAR(DEV_GRP_P1, 0x10, RES_STATE_WRST), 15},
-	{MSG_SINGULAR(DEV_GRP_P1, 0x7, RES_STATE_WRST), 0x60},
-	{MSG_SINGULAR(DEV_GRP_P1, 0x19, RES_STATE_ACTIVE), 2},
-	{MSG_SINGULAR(DEV_GRP_NULL, 0x1b, RES_STATE_ACTIVE), 2},
-};
-
-static struct twl4030_script wrst_script __initdata = {
-	.script = wrst_seq,
-	.size   = ARRAY_SIZE(wrst_seq),
-	.flags  = TWL4030_WRST_SCRIPT,
-};
-
-static struct twl4030_script *twl4030_scripts[] __initdata = {
-	&sleep_on_script,
-	&wakeup_script,
-	&wrst_script,
-};
-
-static struct twl4030_resconfig twl4030_rconfig[] = {
-	{ .resource = RES_HFCLKOUT, .devgroup = DEV_GRP_P3, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_VDD1, .devgroup = DEV_GRP_P1, .type = 1,
-		.type2 = -1, .remap_sleep = RES_STATE_OFF },
-	{ .resource = RES_VDD2, .devgroup = DEV_GRP_P1, .type = 1,
-		.type2 = -1, .remap_sleep = RES_STATE_OFF },
-	{ .resource = RES_VPLL1, .devgroup = DEV_GRP_P1, .type = 1,
-		.type2 = -1, .remap_sleep = RES_STATE_OFF },
-	{ .resource = RES_VINTANA2, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_VIO, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_REGEN, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_NRES_PWRON, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_CLKEN, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_SYSEN, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_32KCLKOUT, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_RESET, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ .resource = RES_Main_Ref, .devgroup = DEV_GRP_ALL, .type = 1,
-		.type2 = -1, .remap_sleep = -1 },
-	{ 0, 0 },
-};
-
-static struct twl4030_power_data zoom2_t2scripts_data __initdata = {
-	.scripts	= twl4030_scripts,
-	.num		= ARRAY_SIZE(twl4030_scripts),
-	.resource_config = twl4030_rconfig,
-};
-
-static struct twl4030_platform_data zoom2_twldata = {
-	.irq_base	= TWL4030_IRQ_BASE,
-	.irq_end	= TWL4030_IRQ_END,
-
-	/* platform_data for children goes here */
-	.bci		= &zoom2_bci_data,
-	.madc		= &zoom2_madc_data,
-	.usb		= &zoom2_usb_data,
-	.gpio		= &zoom2_gpio_data,
-	.keypad		= &zoom2_kp_twl4030_data,
-	.power		= &zoom2_t2scripts_data,
-	.vmmc1          = &zoom2_vmmc1,
-	.vmmc2          = &zoom2_vmmc2,
-	.vsim           = &zoom2_vsim,
-
-};
-
-static struct i2c_board_info __initdata zoom2_i2c_boardinfo[] = {
-	{
-		I2C_BOARD_INFO("twl4030", 0x48),
-		.flags		= I2C_CLIENT_WAKE,
-		.irq		= INT_34XX_SYS_NIRQ,
-		.platform_data	= &zoom2_twldata,
-	},
-};
-
-static void synaptics_dev_init(void)
-{
-	/* Set the ts_gpio pin mux */
-	omap_cfg_reg(H18_34XX_GPIO163);
-
-	if (gpio_request(OMAP_SYNAPTICS_GPIO, "touch") < 0) {
-		printk(KERN_ERR "can't get synaptics pen down GPIO\n");
-		return;
-	}
-	gpio_direction_input(OMAP_SYNAPTICS_GPIO);
-	omap_set_gpio_debounce(OMAP_SYNAPTICS_GPIO, 1);
-	omap_set_gpio_debounce_time(OMAP_SYNAPTICS_GPIO, 0xa);
-}
-
-static int synaptics_power(int power_state)
-{
-	/* TODO: synaptics is powered by vbatt */
-	return 0;
-}
-
-static struct synaptics_i2c_rmi_platform_data synaptics_platform_data[] = {
-	{
-		.version	= 0x0,
-		.power		= &synaptics_power,
-		.flags		= SYNAPTICS_SWAP_XY,
-		.irqflags	= IRQF_TRIGGER_LOW,
-	}
-};
-
-static struct i2c_board_info __initdata zoom2_i2c_boardinfo2[] = {
-	{
-		I2C_BOARD_INFO(SYNAPTICS_I2C_RMI_NAME,  0x20),
-		.platform_data = &synaptics_platform_data,
-		.irq = OMAP_GPIO_IRQ(OMAP_SYNAPTICS_GPIO),
-	},
-};
-
-static int __init omap_i2c_init(void)
-{
-	omap_register_i2c_bus(1, 2600, zoom2_i2c_boardinfo,
-			ARRAY_SIZE(zoom2_i2c_boardinfo));
-	omap_register_i2c_bus(2, 400, NULL, 0);
-	omap_register_i2c_bus(3, 400, NULL, 0);
-	return 0;
-}
-
-extern int __init omap_zoom2_debugboard_init(void);
-
-static void enable_board_wakeup_source(void)
-{
-	omap_cfg_reg(AF26_34XX_SYS_NIRQ);
-}
-
-static void __init omap_zoom2_init(void)
-{
-	omap_i2c_init();
-	synaptics_dev_init();
-	omap_serial_init();
-	omap_zoom2_debugboard_init();
-	usb_musb_init();
-	enable_board_wakeup_source();
-	zoom_flash_init();
-}
-
-static void __init omap_zoom2_map_io(void)
+static void __init omap_zoom_map_io(void)
 {
 	omap2_set_globals_343x();
 	omap2_map_common_io();
 }
 
+static struct omap_board_config_kernel zoom_config[] __initdata = {
+};
+
+static void __init omap_zoom_init_irq(void)
+{
+
+	omap_board_config = zoom_config;
+	omap_board_config_size = ARRAY_SIZE(zoom_config);
+
+	omap2_init_common_hw(mt46h32m32lf6_sdrc_params,
+				mt46h32m32lf6_sdrc_params, omap3_mpu_rate_table,
+				omap3_dsp_rate_table, omap3_l3_rate_table);
+	omap_init_irq();
+	omap_gpio_init();
+}
+
+extern int __init omap_zoom2_debugboard_init(void);
+extern void __init zoom_peripherals_init(void);
+
+static void __init omap_zoom_init(void)
+{
+	zoom_peripherals_init();
+	omap_zoom2_debugboard_init();
+}
+
 MACHINE_START(OMAP_ZOOM2, "OMAP Zoom2 board")
-	.phys_io	= 0x48000000,
-	.io_pg_offst	= ((0xfa000000) >> 18) & 0xfffc,
-	.boot_params	= 0x80000100,
-	.map_io		= omap_zoom2_map_io,
-	.init_irq	= omap_zoom2_init_irq,
-	.init_machine	= omap_zoom2_init,
-	.timer		= &omap_timer,
+	.phys_io        = 0x48000000,
+	.io_pg_offst    = ((0xfa000000) >> 18) & 0xfffc,
+	.boot_params    = 0x80000100,
+	.map_io         = omap_zoom_map_io,
+	.init_irq       = omap_zoom_init_irq,
+	.init_machine   = omap_zoom_init,
+	.timer          = &omap_timer,
 MACHINE_END
