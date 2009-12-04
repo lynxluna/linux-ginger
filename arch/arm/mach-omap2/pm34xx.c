2d55075c5472b21364d3e55b0bd55eaec5d212ce
Shweta Gulati <shweta.gulati@ti.com>
2009-12-04 16:47:37 +0530

    OMAP3 PM: Fix for DSP Crash at OPP 1 and 2 under DVFS+SR
    operation
    
    DSP usage at VDD1 OPP1 and OPP2 with Smartreflex enabled and any MM UCs
    running DSP codec was earlier restricted as DSP crashed.
    The root cause is wrong DPLL1/DPLL2 Bypass clock at VDD1 OPP1 and OPP2.
    The workaround is:
    	For DPLL2 (DSP) select CORECLK/4 as DPLL2 bypass clock for 3430.
    	Select CORECLK/2 as DPLL2 bypass clock for 3630.
    During DPLL2 relock phase, DSP clock will be 83MHz/200MHz which is always OK
    irrespective of Vdd1 voltage.
    For DPLL1 (MPU), prior to any DVFS transition to OPP1, select CORECLK/4
    	(CORECLK/2 for 3630) as DPLL1 bypass clock.
    	For other OPPs select CORECLK/2 (CORECLK/1 for 3630) as DPLL1 bypass
    	clock.
    These configurations are typically set in bootloader. However bootloaders may
    mess up configuration and kernel with this chang ensures that system is in a
    known state.
    
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>
    Signed-off-by: Shweta Gulati <shweta.gulati@ti.com>
    Signed-off-by: Manjunatha GK <manjugk@ti.com>

--- arch/arm/mach-omap2/pm34xx.c
+++ arch/arm/mach-omap2/pm34xx.c
@@ -812,6 +812,26 @@ static void __init omap3_d2d_idle(void)
 
 static void __init prcm_setup_regs(void)
 {
+	u32 cm_clksel1_mpu, cm_clksel1_iva2;
+	#warning TODO : Fix checkpatch warnings for arch/arm/mach-omap2/pm34xx.c
+
+	/*set Bypass clock dividers for MPU and IVA */
+	cm_clksel1_mpu = cm_read_mod_reg(MPU_MOD, CM_CLKSEL1);
+	cm_clksel1_iva2 = cm_read_mod_reg(OMAP3430_IVA2_MOD, CM_CLKSEL1);
+	if (cpu_is_omap3630()) {
+		cm_clksel1_iva2 = (cm_clksel1_iva2 & ~(OMAP3430_IVA2_CLK_SRC_MASK)) |
+					(0x2 << OMAP3430_IVA2_CLK_SRC_SHIFT);
+		cm_clksel1_mpu = (cm_clksel1_mpu & ~(OMAP3430_MPU_CLK_SRC_MASK)) |
+					(0x1 << OMAP3430_MPU_CLK_SRC_SHIFT);
+	} else if (cpu_is_omap34xx()) {
+		cm_clksel1_iva2 = (cm_clksel1_iva2 & ~(OMAP3430_IVA2_CLK_SRC_MASK)) |
+					(0x4 << OMAP3430_IVA2_CLK_SRC_SHIFT);
+		cm_clksel1_mpu = (cm_clksel1_mpu & ~(OMAP3430_MPU_CLK_SRC_MASK)) |
+					(0x2 << OMAP3430_MPU_CLK_SRC_SHIFT);
+		}
+	cm_write_mod_reg(cm_clksel1_iva2, OMAP3430_IVA2_MOD, CM_CLKSEL1);
+	cm_write_mod_reg(cm_clksel1_mpu, MPU_MOD, CM_CLKSEL1);
+
 	/* XXX Reset all wkdeps. This should be done when initializing
 	 * powerdomains */
 	prm_write_mod_reg(0, OMAP3430_IVA2_MOD, PM_WKDEP);
