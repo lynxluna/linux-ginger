0f9438d6b84e956fa83801069a56ff8d78e15003
Janakiram Sistla <x0110377@ti.com>
2010-01-19 14:48:35 +0530

    omap: disable GAIA I2C1/I2C4 internal pull-ups
    This patch disables GAIA I2C1 and I2C4(SR) internal pull-up, to
    use only the external HW resitor >= 470 Ohm for the assured
    functionality in HS mode.
    
    While testing the I2C in High Speed mode, it was discovered that
    without a proper pull-up resitor, there is data corruption during
    multi-byte transfers. RTC(time_set) test case was used for testing.
    
    From the analysis done, it was concluded that ideally we need a pull-up
    of 1.6 K Ohm (recomended) or atleast 470 Ohm or greater for assured
    performance in HS mode.
    
    Signed-off-by: Moiz Sonasath <m-sonasath@ti.com>
    Signed-off-by: Allen Pais <Allen.pais@ti.com>
    Signed-off-by: Janakiram Sistla <x0110377@ti.com>
    Signed-off-by: Manjunatha GK <manjugk@ti.com>

--- include/linux/i2c/twl.h
+++ include/linux/i2c/twl.h
@@ -170,6 +170,20 @@ int twl6030_interrupt_mask(u8 bit_mask, u8 offset);
 
 /*----------------------------------------------------------------------*/
 
+/* Iterface Bit Register (INTBR) offsets
+ * (use TWL4030_MODULE_INTBR)
+ */
+
+#define REG_GPPUPDCTR1			0x0F
+
+/* I2C1 and I2C4(SR) SDA/SCL pull-up control bits */
+
+#define I2C_SCL_CTRL_PU			(1 << 0)
+#define I2C_SDA_CTRL_PU			(1 << 2)
+#define SR_I2C_SCL_CTRL_PU		(1 << 4)
+#define SR_I2C_SDA_CTRL_PU		(1 << 6)
+/*----------------------------------------------------------------------*/
+
 /*
  * NOTE:  at up to 1024 registers, this is a big chip.
  *
