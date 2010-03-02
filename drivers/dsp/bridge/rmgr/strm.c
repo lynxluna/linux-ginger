31996a4b61c768919d273d8665ea86b720680146
Omar Ramirez Luna <omar.ramirez@ti.com>
2010-03-02 16:57:51 -0600

    Revert "DSPBRIDGE: Validate stream handle from user."
    This reverts commit 2a54b6e9d37dc5d49003dfcfd84e1c28e507cbf2.
    
    Conflicts:
    
    	drivers/dsp/bridge/pmgr/wcd.c

--- drivers/dsp/bridge/rmgr/strm.c
+++ drivers/dsp/bridge/rmgr/strm.c
@@ -121,11 +121,16 @@ DSP_STATUS STRM_AllocateBuffer(struct STRM_OBJECT *hStrm, u32 uSize,
 	GT_4trace(STRM_debugMask, GT_ENTER, "STRM_AllocateBuffer: hStrm: 0x%x\t"
 		 "uSize: 0x%x\tapBuffer: 0x%x\tuNumBufs: 0x%x\n",
 		 hStrm, uSize, apBuffer, uNumBufs);
-	/*
-	 * Allocate from segment specified at time of stream open.
-	 */
-	if (uSize == 0)
-		status = DSP_ESIZE;
+	if (MEM_IsValidHandle(hStrm, STRM_SIGNATURE)) {
+		/*
+		 * Allocate from segment specified at time of stream open.
+		 */
+		if (uSize == 0)
+			status = DSP_ESIZE;
+
+	} else {
+		status = DSP_EHANDLE;
+	}
 
 	if (DSP_FAILED(status))
 		goto func_end;
@@ -174,17 +179,20 @@ DSP_STATUS STRM_Close(struct STRM_OBJECT *hStrm,
 
 	GT_1trace(STRM_debugMask, GT_ENTER, "STRM_Close: hStrm: 0x%x\n", hStrm);
 
+	if (!MEM_IsValidHandle(hStrm, STRM_SIGNATURE)) {
+		status = DSP_EHANDLE;
+	} else {
+		/* Have all buffers been reclaimed? If not, return
+		 * DSP_EPENDING */
+		pIntfFxns = hStrm->hStrmMgr->pIntfFxns;
+		status = (*pIntfFxns->pfnChnlGetInfo) (hStrm->hChnl, &chnlInfo);
+		DBC_Assert(DSP_SUCCEEDED(status));
 
-	/* Have all buffers been reclaimed? If not, return
-	 * DSP_EPENDING */
-	pIntfFxns = hStrm->hStrmMgr->pIntfFxns;
-	status = (*pIntfFxns->pfnChnlGetInfo) (hStrm->hChnl, &chnlInfo);
-	DBC_Assert(DSP_SUCCEEDED(status));
-
-	if (chnlInfo.cIOCs > 0 || chnlInfo.cIOReqs > 0)
-		status = DSP_EPENDING;
-	else
-		status = DeleteStrm(hStrm);
+		if (chnlInfo.cIOCs > 0 || chnlInfo.cIOReqs > 0)
+			status = DSP_EPENDING;
+		else
+			status = DeleteStrm(hStrm);
+	}
 
 	if (DSP_FAILED(status))
 		goto func_end;
@@ -306,23 +314,26 @@ DSP_STATUS STRM_FreeBuffer(struct STRM_OBJECT *hStrm, u8 **apBuffer,
 	GT_3trace(STRM_debugMask, GT_ENTER, "STRM_FreeBuffer: hStrm: 0x%x\t"
 		 "apBuffer: 0x%x\tuNumBufs: 0x%x\n", hStrm, apBuffer, uNumBufs);
 
-	for (i = 0; i < uNumBufs; i++) {
-		DBC_Assert(hStrm->hXlator != NULL);
-		status = CMM_XlatorFreeBuf(hStrm->hXlator, apBuffer[i]);
-		if (DSP_FAILED(status)) {
-			GT_0trace(STRM_debugMask, GT_7CLASS,
-				 "STRM_FreeBuffer: DSP_FAILED"
-				 " to free shared memory.\n");
-			break;
-		}
-		apBuffer[i] = NULL;
-	}
+	if (!MEM_IsValidHandle(hStrm, STRM_SIGNATURE))
+		status = DSP_EHANDLE;
 
 	if (DSP_SUCCEEDED(status)) {
-		if (DRV_GetSTRMResElement(hStrm, hSTRMRes, pr_ctxt) !=
-				DSP_ENOTFOUND)
-			DRV_ProcUpdateSTRMRes(uNumBufs-i, hSTRMRes);
+		for (i = 0; i < uNumBufs; i++) {
+			DBC_Assert(hStrm->hXlator != NULL);
+			status = CMM_XlatorFreeBuf(hStrm->hXlator, apBuffer[i]);
+			if (DSP_FAILED(status)) {
+				GT_0trace(STRM_debugMask, GT_7CLASS,
+					 "STRM_FreeBuffer: DSP_FAILED"
+					 " to free shared memory.\n");
+				break;
+			}
+			apBuffer[i] = NULL;
+		}
 	}
+	if (DRV_GetSTRMResElement(hStrm, hSTRMRes, pr_ctxt) !=
+			DSP_ENOTFOUND)
+		DRV_ProcUpdateSTRMRes(uNumBufs-i, hSTRMRes);
+
 	return status;
 }
 
@@ -347,12 +358,14 @@ DSP_STATUS STRM_GetInfo(struct STRM_OBJECT *hStrm,
 	GT_3trace(STRM_debugMask, GT_ENTER, "STRM_GetInfo: hStrm: 0x%x\t"
 		 "pStreamInfo: 0x%x\tuStreamInfoSize: 0x%x\n", hStrm,
 		 pStreamInfo, uStreamInfoSize);
-
-	if (uStreamInfoSize < sizeof(struct STRM_INFO)) {
-		/* size of users info */
-		status = DSP_ESIZE;
+	if (!MEM_IsValidHandle(hStrm, STRM_SIGNATURE)) {
+		status = DSP_EHANDLE;
+	} else {
+		if (uStreamInfoSize < sizeof(struct STRM_INFO)) {
+			/* size of users info */
+			status = DSP_ESIZE;
+		}
 	}
-
 	if (DSP_FAILED(status))
 		goto func_end;
 
@@ -407,11 +420,14 @@ DSP_STATUS STRM_Idle(struct STRM_OBJECT *hStrm, bool fFlush)
 	GT_2trace(STRM_debugMask, GT_ENTER, "STRM_Idle: hStrm: 0x%x\t"
 		 "fFlush: 0x%x\n", hStrm, fFlush);
 
-	pIntfFxns = hStrm->hStrmMgr->pIntfFxns;
+	if (!MEM_IsValidHandle(hStrm, STRM_SIGNATURE)) {
+		status = DSP_EHANDLE;
+	} else {
+		pIntfFxns = hStrm->hStrmMgr->pIntfFxns;
 
-	status = (*pIntfFxns->pfnChnlIdle) (hStrm->hChnl,
-		 hStrm->uTimeout, fFlush);
-
+		status = (*pIntfFxns->pfnChnlIdle) (hStrm->hChnl,
+			 hStrm->uTimeout, fFlush);
+	}
 	return status;
 }
 
@@ -462,24 +478,26 @@ DSP_STATUS STRM_Issue(struct STRM_OBJECT *hStrm, IN u8 *pBuf, u32 ulBytes,
 	GT_4trace(STRM_debugMask, GT_ENTER, "STRM_Issue: hStrm: 0x%x\tpBuf: "
 		 "0x%x\tulBytes: 0x%x\tdwArg: 0x%x\n", hStrm, pBuf, ulBytes,
 		 dwArg);
+	if (!MEM_IsValidHandle(hStrm, STRM_SIGNATURE)) {
+		status = DSP_EHANDLE;
+	} else {
+		pIntfFxns = hStrm->hStrmMgr->pIntfFxns;
 
-	pIntfFxns = hStrm->hStrmMgr->pIntfFxns;
+		if (hStrm->uSegment != 0) {
+			pTmpBuf = CMM_XlatorTranslate(hStrm->hXlator,
+					(void *)pBuf, CMM_VA2DSPPA);
+			if (pTmpBuf == NULL)
+				status = DSP_ETRANSLATE;
 
-	if (hStrm->uSegment != 0) {
-		pTmpBuf = CMM_XlatorTranslate(hStrm->hXlator,
-				(void *)pBuf, CMM_VA2DSPPA);
-		if (pTmpBuf == NULL)
-			status = DSP_ETRANSLATE;
-
+		}
+		if (DSP_SUCCEEDED(status)) {
+			status = (*pIntfFxns->pfnChnlAddIOReq)
+				 (hStrm->hChnl, pBuf, ulBytes, ulBufSize,
+				 (u32) pTmpBuf, dwArg);
+		}
+		if (status == CHNL_E_NOIORPS)
+			status = DSP_ESTREAMFULL;
 	}
-	if (DSP_SUCCEEDED(status)) {
-		status = (*pIntfFxns->pfnChnlAddIOReq)
-			 (hStrm->hChnl, pBuf, ulBytes, ulBufSize,
-			 (u32) pTmpBuf, dwArg);
-	}
-	if (status == CHNL_E_NOIORPS)
-		status = DSP_ESTREAMFULL;
-
 	return status;
 }
 
@@ -673,6 +691,10 @@ DSP_STATUS STRM_Reclaim(struct STRM_OBJECT *hStrm, OUT u8 **pBufPtr,
 		 "\tpulBytes: 0x%x\tpdwArg: 0x%x\n", hStrm, pBufPtr, pulBytes,
 		 pdwArg);
 
+	if (!MEM_IsValidHandle(hStrm, STRM_SIGNATURE)) {
+		status = DSP_EHANDLE;
+		goto func_end;
+	}
 	pIntfFxns = hStrm->hStrmMgr->pIntfFxns;
 
 	status = (*pIntfFxns->pfnChnlGetIOC)(hStrm->hChnl, hStrm->uTimeout,
@@ -724,6 +746,7 @@ DSP_STATUS STRM_Reclaim(struct STRM_OBJECT *hStrm, OUT u8 **pBufPtr,
 		}
 		*pBufPtr = chnlIOC.pBuf;
 	}
+func_end:
 	/* ensure we return a documented return code */
 	DBC_Ensure(DSP_SUCCEEDED(status) || status == DSP_EHANDLE ||
 		  status == DSP_ETIMEOUT || status == DSP_ETRANSLATE ||
@@ -750,8 +773,9 @@ DSP_STATUS STRM_RegisterNotify(struct STRM_OBJECT *hStrm, u32 uEventMask,
 		 "STRM_RegisterNotify: hStrm: 0x%x\t"
 		 "uEventMask: 0x%x\tuNotifyType: 0x%x\thNotification: 0x%x\n",
 		 hStrm, uEventMask, uNotifyType, hNotification);
-
-	if ((uEventMask & ~((DSP_STREAMIOCOMPLETION) |
+	if (!MEM_IsValidHandle(hStrm, STRM_SIGNATURE)) {
+		status = DSP_EHANDLE;
+	} else if ((uEventMask & ~((DSP_STREAMIOCOMPLETION) |
 		 DSP_STREAMDONE)) != 0) {
 		status = DSP_EVALUE;
 	} else {
